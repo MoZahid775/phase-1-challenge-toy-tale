4c4df868eeba0138cbf906cca6394ded47c08bd2
Muhammad Zahid <muhammadali775@gmail.com>
2021-05-05 17:46:47 -0400

    Fetching and rendering toys to Dom

--- src/index.js
+++ src/index.js
@@ -12,4 +12,39 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+
+  //On the index.html page, there is a div with the id "toy-collection."
+//When the page loads, make a 'GET' request to fetch all the toy objects. With the response data, make a <div class="card"> for each toy and add it to the toy-collection div.
+
+
+fetch('http://localhost:3000/toys') 
+.then(res => res.json())
+.then((toysArray) => (toysArray.forEach(toy => {renderToy(toy)})));
+
+  // fetch("http://localhost:4000/articles")
+  //   .then((r) => r.json())
+  //   .then((articlesArray) => console.log(articlesArray));
 });
+
+// Function of Rendering a Toy by creating a <div>
+
+function renderToy(toy){
+
+ let div = document.createElement('div')
+ div.className = 'card'
+ div.innerHTML = `
+ <h2>${toy.name}</h2>
+ <img src=${toy.image} class="toy-avatar">
+ <p>${toy.likes}</p>
+<button class="like-btn" id=${toy.id}>Like <3</button>`
+
+document.getElementById('toy-collection').appendChild(div)
+
+}
+
+
+
+
+
